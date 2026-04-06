eb349826b2b5efed117e3f28fe82133cf3cf1d1c
Rohitrajashekhar22 <rohit66523@gmail.com>
2026-04-06 21:26:17 +0530

    fix vercel routing

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://finance-tracker-34yx.onrender.com/api/transactions",
+  baseURL: "https://finance-tracker-34yx.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
